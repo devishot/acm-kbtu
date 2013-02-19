1cb871bf32ca7dbffc9e72c40e8556b6801017ba
Devishot <sattar.coder@gmail.com>
2013-02-19 12:22:42 +0600

    Compiler: freepascal compiler added, but need to catch compile errors

--- judge-files/check-system/compiler.rb
+++ judge-files/check-system/compiler.rb
@@ -1,7 +1,21 @@
 module Compiler
   def Compiler.compile(ufile)
-    pid, stdin, stdout, stderr = Open4::popen4 "g++ \'#{File.path(ufile)}\' -o \'#{File.dirname(ufile)}/checker.exe\'"
-    compile_err = stderr.readlines #we need to save, it will changed
+    if File.extname(ufile) == ".cpp"
+      pid, stdin, stdout, stderr = 
+        Open4::popen4 "g++ \'#{File.path(ufile)}\' -o \'#{File.dirname(ufile)}/checker.exe\'"
+      compile_err = stderr.readlines #we need to save, it will changed
+    elsif File.extname(ufile) == ".pas"
+      pid, stdin, stdout, stderr = 
+        Open4::popen4 "fpc \'#{File.path(ufile)}\' -o\'#{File.dirname(ufile)}/checker.exe\'"
+      #puts "#{pid} #{stdout.readlines.inspect} #{stderr.readlines.inspect}"
+      compile_err = stderr.readlines #we need to save, it will changed
+      compile_err.each_with_index do |value,index|
+        #value.include? "warning:"
+      end
+    else
+      compile_err = ["file extension not supported."]
+    end
+      
 
     if compile_err.blank?
       return {'status' => 'OK'}
---
+++ judge-files/contests/1/checker/d.pas
@@ -0,0 +1,6 @@
+Program Lesson1_Program1;   
+Begin
+2w    
+ Write('Hello World. Prepare to learn PASCAL!!');
+ Readln
+End.
