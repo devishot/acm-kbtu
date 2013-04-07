e7c1c0ed172cb3ea2544a528eb2b184a90d78daf
devishot <sattar.coder@gmail.com>
2013-04-07 22:43:25 +0600

    gitignore files deleted

--- judge-files/contests/1/checker/d.pas
+++
@@ -1,6 +0,0 @@
-Program Lesson1_Program1;   
-Begin
-2w    
- Write('Hello World. Prepare to learn PASCAL!!');
- Readln
-End.
--- judge-files/contests/1/checker/e.pas
+++
@@ -1,5 +0,0 @@
-Program Lesson1_Program1;   
-Begin   
- Write('Hello World. Prepare to learn PASCAL!!');
- Readln;
-End.
