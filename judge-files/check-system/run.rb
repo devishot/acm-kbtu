d5a26e1fcb87c047040eaee21c1d10f20fe904de
devishot <sattar.coder@gmail.com>
2013-06-10 16:08:24 +0600

    Resque bug2

--- judge-files/check-system/run.rb
+++ judge-files/check-system/run.rb
@@ -11,7 +11,7 @@ class Tester
   def initialize(submit_id, hidden=false)
     @init_status = false
 
-    raise Submit.all.inspect
+    raise "#{Submit.all[0..-1]} | #{submit_id}"
     @submit = Submit.find(submit_id)
     @submit.status = {:status => '', :error => [], :test => ''}
     @submit.tests_status = [{}] #empty, full in Run
