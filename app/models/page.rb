70096ce06f06a4b5cc0e58b7f04f01e07063e1b0
devishot <sattar.coder@gmail.com>
2013-06-12 17:00:07 +0600

    Page: set path fixed

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,11 +4,17 @@ class Page
 
   field :text_title, type: String
   field :text_body, type: String
-  field :path, type: String, default: (Page.exists? ? (Page.last.path.to_i() + 1).to_s() : '1000')
+  field :path, type: String
   field :order, type: Integer
   field :old_node
 
   belongs_to :node
   belongs_to :user
+
+  before_create :set_path
+
+  def set_path
+   self.path = (Page.where(node: self.node).count + 1).to_s()
+  end
   
 end
